af8a8e8eadb769fad1e8e9efa623b8b9f776b9c9
Daniel <daniel.zam29@gmail.com>
2024-07-19 17:27:47 -0600

    prueba2 mision

--- frontend/adopetme/src/components/Home/Mision&Vision.jsx
+++ frontend/adopetme/src/components/Home/Mision&Vision.jsx
@@ -1,10 +1,58 @@
-import { Box } from "@mui/material";
+//Material UI
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
-export default function Mision() {
-  
+//Icosn
+import AddHomeIcon from "@mui/icons-material/AddHome";
 
-  return ( <Box><Typography variant="h1" color="initial">Mision</Typography></Box>
-    
+import logo from "../shared/logo-1.png";
+
+export default function Mision() {
+  return (
+    <Box
+      sx={{
+        bgcolor: "cyan",
+      }}
+    >
+      {/* <Box sx={{ bgcolor: "bisque" }}>
+        <Typography variant="" color="initial">
+          BOX1
+        </Typography>
+      </Box>
+      <Box sx={{ bgcolor: "blueviolet" }}>
+        <Typography variant="" color="initial">
+          BOX2
+        </Typography>
+      </Box> */}
+      <Stack
+        sx={{ bgcolor: "GrayText" }}
+        flexDirection={"row"}
+        justifyContent={"space-around"}
+      >
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <Typography variant="" color="secondary">
+            Mision
+          </Typography>
+          <Typography variant="" color="white">
+            gergekjsoeifj fjaweifpjaweipfjaewip fawijpfjsipajf aipsjfasiofj ip
+          </Typography>
+          <Avatar
+            alt="Remy Sharp"
+            src={logo}
+            sx={{ width: 200, height: 200 }}
+            variant="square"
+          />
+          <AddHomeIcon color="success" fontSize="300" />
+        </Box>
+
+        <Box>
+          <Typography variant="" color="initial">
+            Vision
+          </Typography>
+        </Box>
+      </Stack>
+    </Box>
   );
 }
