8fcfcafce208fa3d52a4b9155abd1c9b042f4be9
Jhon Serna <jhonfserna@yahoo.com>
2024-07-19 08:09:51 -0500

    Se han agregado header, banner y footer

--- frontend/adopetme/src/Pages/Home/index.jsx
+++ frontend/adopetme/src/Pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../../components/Home/Header";
+import Banner from "../../components/Home/Banner";
 import CardsPetsMain from "../../components/Home/CardsPetsMain";
+import Footer from "../../components/Home/Footer";
 
 //Components
 // import Banner from "../../components/Home/Banner";
@@ -9,7 +11,9 @@ const index = () => {
   return (
     <div>
       <Header />
+      <Banner/>
       <CardsPetsMain />
+      <Footer/>
     </div>
   );
 };
