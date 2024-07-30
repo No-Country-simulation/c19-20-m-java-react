7263ce461b71a9af132b815b73aff5555934e292
Daniel <daniel.zam29@gmail.com>
2024-07-30 09:09:22 -0600

    ajuste router

--- frontend/adopetme/src/App.js
+++ frontend/adopetme/src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home/Home'; // Ajusta la ruta según tu estructura de carpetas
-import UserProfile from './components/User/UserProfile'; // Ajusta la ruta según tu estructura de carpetas
+import React from "react";
+//import { Routes, Route } from "react-router-dom";
+//import Home from "./components/Home/Home"; // Ajusta la ruta según tu estructura de carpetas
+//import UserProfile from "./components/User/UserProfile"; // Ajusta la ruta según tu estructura de carpetas
+import MainRouter from "./routers/MainRouter";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<UserProfile />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<Home />} />
+    //   <Route path="/profile" element={<UserProfile />} />
+    // </Routes>
+    <MainRouter />
   );
 }
 
 export default App;
-
-
-
--- frontend/adopetme/src/routers/MainRouter.jsx
+++ frontend/adopetme/src/routers/MainRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { SearchProvider } from '../../contexts/SearchContext';
+//import { SearchProvider } from '../../contexts/SearchContext';
 
 //Components
 import Home from "../Pages/Home/index";
@@ -9,22 +9,26 @@ import MainLayout from "../Pages/layout/MainLayout";
 import AdoptPets from "../components/Pets/AdoptPets";
 import PetForm from "../Pages/PetForm";
 import UserProfile from "../components/User/UserProfile";
-
+import { SearchProvider } from "../contexts/SearchContext";
+import { AuthProvider } from "../contexts/AuthContext";
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
-      <SearchProvider> {/* Proveer el contexto */}
-        <MainLayout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/publicaciones" element={<PublicationsPets />} />
-            <Route path="/adopcion" element={<AdoptPets />} />
-            <Route path="/registro-mascotas" element={<PetForm />} />
-            <Route path="/profile" element={<UserProfile/>} />
-          </Routes>
-        </MainLayout>
-      </SearchProvider>
+      <AuthProvider>
+        <SearchProvider>
+          {/* Proveer el contexto */}
+          <MainLayout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/publicaciones" element={<PublicationsPets />} />
+              <Route path="/adopcion" element={<AdoptPets />} />
+              <Route path="/registro-mascotas" element={<PetForm />} />
+              <Route path="/profile" element={<UserProfile />} />
+            </Routes>
+          </MainLayout>
+        </SearchProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
