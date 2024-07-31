64909fe1d110c512aca85f3a6d889085dbfe0bcd
Jhoan Sebastian Urrea Sanchez <js.urrea03@gmail.com>
2024-07-31 11:29:38 -0500

    nuevos cambios de ubicacion

--- backend/user-details-service/src/main/java/com/adopetme/user_details_service/domain/service/LocationDetailsService.java
+++ backend/user-details-service/src/main/java/com/adopetme/user_details_service/domain/service/LocationDetailsService.java
@@ -10,12 +10,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class LocationDetailsService {
 
+    @Autowired
     private final CountryServiceClient countryServiceClient;
 
     @Autowired
     public LocationDetailsService(CountryServiceClient countryServiceClient) {
         this.countryServiceClient = countryServiceClient;
     }
+
     public AllNames getLocationNamesById(Long countryId, Long stateId, Long cityId) {
         ResponseEntity<AllNames> response = countryServiceClient.getNamesById(countryId, stateId, cityId);
         return response.getBody();
--- backend/user-details-service/src/main/java/com/adopetme/user_details_service/persistence/webclient/CountryServiceClient.java
+++ backend/user-details-service/src/main/java/com/adopetme/user_details_service/persistence/webclient/CountryServiceClient.java
@@ -7,14 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "country-service", url = "http://localhost:433/country-service")
+@FeignClient(name = "country-service", url = "http://localhost:8087")
 public interface CountryServiceClient {
 
-    @GetMapping("/idsbyname/{namecountry}/{namestate}/{namecity}")
+    @GetMapping("/country/idsbyname/{namecountry}/{namestate}/{namecity}")
     ResponseEntity<IdsAll> getIdsByName(@PathVariable("namecountry") String nameCountry,
                                         @PathVariable("namestate") String nameState,
                                         @PathVariable("namecity") String nameCity);
-    @GetMapping("/namesbyid/{idcountry}/{idstate}/{idcity}")
+    @GetMapping("/country/namesbyid/{idcountry}/{idstate}/{idcity}")
     ResponseEntity<AllNames> getNamesById(@PathVariable("idcountry") Long idCountry,
                                              @PathVariable("idstate") Long idState,
                                              @PathVariable("idcity") Long idCity);
