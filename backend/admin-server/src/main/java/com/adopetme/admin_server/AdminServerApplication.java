9028c60a23485e65d73fbc3e29ff114b0c64b73e
ajax159 <gmod159@gmail.com>
2024-08-01 14:08:38 -0500

    Add @EnableAdminServer annotation to AdminServerApplication.java and configure server port and security credentials in application.properties

--- backend/admin-server/src/main/java/com/adopetme/admin_server/AdminServerApplication.java
+++ backend/admin-server/src/main/java/com/adopetme/admin_server/AdminServerApplication.java
@@ -3,6 +3,7 @@ package com.adopetme.admin_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer
 @SpringBootApplication
 public class AdminServerApplication {
 
