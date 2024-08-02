1a0da90a2ad73ccabba262c436251a8147819b26
mauricio2517 <mauricio.25.17@gmail.com>
2024-08-02 00:23:03 -0500

    Subiendo la actualizacion del perfil de usuario para mostrar el catalogo de mascotas publicadas por el mismo

--- frontend/adopetme/src/components/User/UserProfile.jsx
+++ frontend/adopetme/src/components/User/UserProfile.jsx
@@ -1,21 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Box,
-  Typography,
-  Avatar,
-  Grid,
-  TextField,
-  Button,
-  Modal,
-  IconButton,
-  Paper,
-  CircularProgress,
-  Alert
+  Box, Typography, Avatar, Grid, TextField, Button, Modal, IconButton, Paper, CircularProgress, Alert, Stack
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { useAuth } from '../../contexts/AuthContext';
 import PetForm from '../../Pages/PetForm';
 import EditProfileModal from './EditProfileModal';
+import axios from 'axios';
+import SkeletonCards from '../Pets/SkeletonCards';
+import CardsPets from '../Pets/CardsPets';
+import converterBase64ToUrl from '../../utils/converterBase64ToUrl';
+import NoPhoto from '../../assets/img/nophoto.png';
 
 const UserProfile = () => {
   const { user, fetchUserDetails } = useAuth();
@@ -23,20 +18,44 @@ const UserProfile = () => {
   const [loading, setLoading] = useState(true);
   const [isPetFormModalOpen, setIsPetFormModalOpen] = useState(false);
   const [isEditProfileModalOpen, setIsEditProfileModalOpen] = useState(false);
+  const [pets, setPets] = useState([]);
   const [message, setMessage] = useState('');
   const [messageType, setMessageType] = useState('');
+  const [idPets, setIdPets] = useState();
 
   useEffect(() => {
-    console.log('User:', user);
     const fetchData = async () => {
       if (user && user.idUserDetails) {
         try {
-          console.log('Fetching data for user:', user);
           const details = await fetchUserDetails(user.idUserDetails);
-          console.log('User details fetched:', details);
           setUserDetails(details);
+
+          const response = await axios.get(`${process.env.REACT_APP_API_URL}/pet/petimage`);
+          if (response.data && Array.isArray(response.data.data)) {
+            const userPets = response.data.data.filter(pet => pet.createdBy === user.idUserDetails);
+
+            const newPets = await Promise.all(userPets.map(async (pet) => {
+              try {
+                const response = await axios.get(
+                  `${process.env.REACT_APP_API_URL}/users_details/${pet.createdBy}`
+                );
+                const result = response.data;
+                return {
+                  ...pet,
+                  ubication: `${result.city}, ${result.state}, ${result.country}`,
+                };
+              } catch (error) {
+                console.error("Failed to fetch user details:", error);
+                return pet;
+              }
+            }));
+
+            setPets(newPets);
+          } else {
+            console.error('Unexpected response format:', response.data);
+          }
         } catch (error) {
-          console.error('Error fetching user details:', error);
+          console.error('Error fetching data:', error);
         } finally {
           setLoading(false);
         }
@@ -64,10 +83,6 @@ const UserProfile = () => {
 
   const handleSaveProfile = async (updatedDetails) => {
     try {
-      // Aquí puedes hacer la llamada a la API para guardar los cambios
-      // Ejemplo: await axios.put(`${process.env.REACT_APP_API_URL}/users_details/${user.idUserDetails}`, updatedDetails);
-
-      // Para este ejemplo, simplemente actualizamos el estado localmente
       setUserDetails(updatedDetails);
       setMessageType('success');
       setMessage('Perfil actualizado con éxito');
@@ -77,6 +92,11 @@ const UserProfile = () => {
       setMessage('Error al actualizar el perfil');
       console.error('Error al actualizar el perfil:', error);
     }
+  };
+
+  const handleClickAdopt = (id) => {
+    setIsPetFormModalOpen(true);
+    setIdPets(id);
   };
 
   if (loading) {
@@ -144,7 +164,7 @@ const UserProfile = () => {
           </Grid>
         </Grid>
       </Paper>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} sx={{ mb: 4 }}>
         <Grid item xs={12}>
           <Button variant="contained" color="primary" fullWidth onClick={handleOpenEditProfileModal}>
             Editar Perfil
@@ -156,6 +176,39 @@ const UserProfile = () => {
           </Button>
         </Grid>
       </Grid>
+      
+      <Typography variant="h6" component="h2" sx={{ mt: 4, mb: 2 }}>
+        Mis Mascotas en Adopción
+      </Typography>
+      <Stack
+        spacing={{
+          xs: 1,
+          sm: 4,
+        }}
+        direction="row"
+        useFlexGap
+        flexWrap="wrap"
+        justifyContent="center"
+        width="80%"
+        mx="auto"
+        sx={{ mt: 4 }}
+      >
+        {loading
+          ? [1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+              <SkeletonCards key={item} />
+            ))
+          : pets.map((pet) => (
+              <CardsPets
+                key={pet.idPet}
+                id={pet.idPet}
+                img={pet.image.length > 0 ? converterBase64ToUrl(pet.image[0].imagePet) : NoPhoto}
+                name={pet.name}
+                gender={pet?.gender}
+                ubication={pet?.ubication}
+                handleClickAdopt={handleClickAdopt}
+              />
+            ))}
+      </Stack>
 
       <Modal open={isPetFormModalOpen} onClose={handleClosePetFormModal}>
         <Box sx={modalStyle}>
