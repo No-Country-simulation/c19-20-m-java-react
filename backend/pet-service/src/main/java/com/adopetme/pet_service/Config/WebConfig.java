4b3a4b922492aa2d91ae729ab5f319a09b014b64
ajax159 <gmod159@gmail.com>
2024-08-01 20:09:41 -0500

    Update dependencies and configuration files

--- backend/pet-service/src/main/java/com/adopetme/pet_service/Config/WebConfig.java
+++ backend/pet-service/src/main/java/com/adopetme/pet_service/Config/WebConfig.java
@@ -1,17 +1,17 @@
-package com.adopetme.pet_service.Config;
+// package com.adopetme.pet_service.Config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowedOrigins("*");
-    }
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
+// @Override
+// public void addCorsMappings(CorsRegistry registry) {
+// registry.addMapping("/**")
+// .allowedMethods("GET", "POST", "PUT", "DELETE")
+// .allowedHeaders("*")
+// .allowedOrigins("*");
+// }
 
-}
+// }
