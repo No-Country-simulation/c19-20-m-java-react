0f4f3c7571303023bad531f657dbc8da3b5c309c
ajax159 <gmod159@gmail.com>
2024-08-02 10:42:55 -0500

    Update SpeciesFeignClient URL

--- backend/pet-service/src/main/java/com/adopetme/pet_service/Client/SpeciesFeignClient.java
+++ backend/pet-service/src/main/java/com/adopetme/pet_service/Client/SpeciesFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.adopetme.pet_service.Dto.GenericResponseRecord;
 import com.adopetme.pet_service.Dto.SpeciesDto;
 
-@FeignClient(name = "specie-service", url = "https://service12.mercelab.com")
+@FeignClient(name = "specie-service", url = "http://localhost:5052")
 public interface SpeciesFeignClient {
 
     @GetMapping(value = "/species/{id}")
