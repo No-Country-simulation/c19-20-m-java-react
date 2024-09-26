af3b5d9846d0da7c2442436402dfb13243642485
Daniel Gonzalez <daniel.zam29@gmail.com>
2024-09-26 16:48:47 -0600

    ajustes en login, ajustes en perfil user

--- frontend/adopetme/src/components/User/UserProfile.jsx
+++ frontend/adopetme/src/components/User/UserProfile.jsx
@@ -1,16 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-  Box, Typography, Avatar, Grid, TextField, Button, Modal, IconButton, Paper, CircularProgress, Alert, Stack
-} from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-import { useAuth } from '../../contexts/AuthContext';
-import PetForm from '../../Pages/PetForm';
-import EditProfileModal from './EditProfileModal';
-import axios from 'axios';
-import SkeletonCards from '../Pets/SkeletonCards';
-import CardsPets from '../Pets/CardsPets';
-import converterBase64ToUrl from '../../utils/converterBase64ToUrl';
-import NoPhoto from '../../assets/img/nophoto.png';
+  Box,
+  Typography,
+  Avatar,
+  Grid,
+  TextField,
+  Button,
+  Modal,
+  IconButton,
+  Paper,
+  CircularProgress,
+  Alert,
+  Stack,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { useAuth } from "../../contexts/AuthContext";
+import PetForm from "../../Pages/PetForm";
+import EditProfileModal from "./EditProfileModal";
+import axios from "axios";
+import SkeletonCards from "../Pets/SkeletonCards";
+import CardsPets from "../Pets/CardsPets";
+import NoPhoto from "../../assets/img/nophoto.png";
 
 const UserProfile = () => {
   const { user, fetchUserDetails } = useAuth();
@@ -19,43 +29,52 @@ const UserProfile = () => {
   const [isPetFormModalOpen, setIsPetFormModalOpen] = useState(false);
   const [isEditProfileModalOpen, setIsEditProfileModalOpen] = useState(false);
   const [pets, setPets] = useState([]);
-  const [message, setMessage] = useState('');
-  const [messageType, setMessageType] = useState('');
-  const [idPets, setIdPets] = useState();
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
+  //const [idPets, setIdPets] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
-      if (user && user.idUserDetails) {
+      if (user && user.id) {
         try {
-          const details = await fetchUserDetails(user.idUserDetails);
+          const details = await fetchUserDetails(user.id);
+
           setUserDetails(details);
 
-          const response = await axios.get(`${process.env.REACT_APP_API_URL}/pet/petimage`);
+          const response = await axios.get(
+            `${process.env.REACT_APP_API_URL}/pet/petimage`
+          );
+
           if (response.data && Array.isArray(response.data.data)) {
-            const userPets = response.data.data.filter(pet => pet.createdBy === user.idUserDetails);
+            const userPets = response.data.data.filter(
+              (pet) => pet.createdBy === user.id
+            );
 
-            const newPets = await Promise.all(userPets.map(async (pet) => {
-              try {
-                const response = await axios.get(
-                  `${process.env.REACT_APP_API_URL}/users_details/${pet.createdBy}`
-                );
-                const result = response.data;
-                return {
-                  ...pet,
-                  ubication: `${result.city}, ${result.state}, ${result.country}`,
-                };
-              } catch (error) {
-                console.error("Failed to fetch user details:", error);
-                return pet;
-              }
-            }));
-
-            setPets(newPets);
+            // const newPets = await Promise.all(
+            //   userPets.map(async (pet) => {
+            //     try {
+            //       const response = await axios.get(
+            //         `${process.env.REACT_APP_API_URL}/auth/users_details/${pet.createdBy}`
+            //       );
+            //       const result = response.data;
+            //       return {
+            //         ...pet,
+            //         ubication: `${result.city}, ${result.state}, ${result.country}`,
+            //       };
+            //     } catch (error) {
+            //       console.error("Failed to fetch user details:", error);
+            //       return pet;
+            //     }
+            //   })
+            // );
+            // console.log("newPets", newPets);
+            //setPets(newPets);
+            setPets(userPets);
           } else {
-            console.error('Unexpected response format:', response.data);
+            console.error("Unexpected response format:", response.data);
           }
         } catch (error) {
-          console.error('Error fetching data:', error);
+          console.error("Error fetching data:", error);
         } finally {
           setLoading(false);
         }
@@ -84,19 +103,19 @@ const UserProfile = () => {
   const handleSaveProfile = async (updatedDetails) => {
     try {
       setUserDetails(updatedDetails);
-      setMessageType('success');
-      setMessage('Perfil actualizado con éxito');
+      setMessageType("success");
+      setMessage("Perfil actualizado con éxito");
       setIsEditProfileModalOpen(false);
     } catch (error) {
-      setMessageType('error');
-      setMessage('Error al actualizar el perfil');
-      console.error('Error al actualizar el perfil:', error);
+      setMessageType("error");
+      setMessage("Error al actualizar el perfil");
+      console.error("Error al actualizar el perfil:", error);
     }
   };
 
   const handleClickAdopt = (id) => {
     setIsPetFormModalOpen(true);
-    setIdPets(id);
+    //setIdPets(id);
   };
 
   if (loading) {
@@ -104,8 +123,12 @@ const UserProfile = () => {
   }
 
   return (
-    <Box sx={{ p: 4, maxWidth: '800px', margin: 'auto' }}>
-      <Typography variant="h4" component="h1" sx={{ mb: 4, textAlign: 'center' }}>
+    <Box sx={{ p: 4, maxWidth: "800px", margin: "auto" }}>
+      <Typography
+        variant="h4"
+        component="h1"
+        sx={{ mb: 4, textAlign: "center" }}
+      >
         Perfil de Usuario
       </Typography>
       {message && (
@@ -114,13 +137,28 @@ const UserProfile = () => {
         </Alert>
       )}
       <Paper elevation={3} sx={{ p: 2, mb: 4 }}>
-        <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+        >
           <Avatar sx={{ width: 120, height: 120, mb: 2 }} />
-          <Typography variant="h6" sx={{ textAlign: 'center' }}>{userDetails?.fullName}</Typography>
-          <Typography variant="body1" color="textSecondary" sx={{ textAlign: 'center' }}>
+          <Typography variant="h6" sx={{ textAlign: "center" }}>
+            {userDetails?.firstname + " " + userDetails?.lastname}
+          </Typography>
+          <Typography
+            variant="body1"
+            color="textSecondary"
+            sx={{ textAlign: "center" }}
+          >
             @{userDetails?.username}
           </Typography>
-          <Typography variant="body1" color="textSecondary" sx={{ textAlign: 'center', mt: 2 }}>
+          <Typography
+            variant="body1"
+            color="textSecondary"
+            sx={{ textAlign: "center", mt: 2 }}
+          >
             {userDetails?.description}
           </Typography>
         </Box>
@@ -134,7 +172,7 @@ const UserProfile = () => {
             <TextField
               fullWidth
               label="Correo Electrónico"
-              value={userDetails?.email || ''}
+              value={userDetails?.email || ""}
               disabled
             />
           </Grid>
@@ -142,7 +180,7 @@ const UserProfile = () => {
             <TextField
               fullWidth
               label="Teléfono"
-              value={userDetails?.phone || ''}
+              value={userDetails?.phone || ""}
               disabled
             />
           </Grid>
@@ -150,7 +188,7 @@ const UserProfile = () => {
             <TextField
               fullWidth
               label="País"
-              value={userDetails?.country || ''}
+              value={userDetails?.country || ""}
               disabled
             />
           </Grid>
@@ -158,7 +196,7 @@ const UserProfile = () => {
             <TextField
               fullWidth
               label="Ciudad"
-              value={userDetails?.city || ''}
+              value={userDetails?.city || ""}
               disabled
             />
           </Grid>
@@ -166,17 +204,27 @@ const UserProfile = () => {
       </Paper>
       <Grid container spacing={2} sx={{ mb: 4 }}>
         <Grid item xs={12}>
-          <Button variant="contained" color="primary" fullWidth onClick={handleOpenEditProfileModal}>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={handleOpenEditProfileModal}
+          >
             Editar Perfil
           </Button>
         </Grid>
         <Grid item xs={12}>
-          <Button variant="contained" color="primary" fullWidth onClick={handleOpenPetFormModal}>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={handleOpenPetFormModal}
+          >
             Registrar Mascota
           </Button>
         </Grid>
       </Grid>
-      
+
       <Typography variant="h6" component="h2" sx={{ mt: 4, mb: 2 }}>
         Mis Mascotas en Adopción
       </Typography>
@@ -189,22 +237,20 @@ const UserProfile = () => {
         useFlexGap
         flexWrap="wrap"
         justifyContent="center"
-        width="80%"
+        //width="80%"
         mx="auto"
         sx={{ mt: 4 }}
       >
         {loading
-          ? [1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
-              <SkeletonCards key={item} />
-            ))
+          ? [1, 2, 3, 4, 5, 6, 7, 8].map((item) => <SkeletonCards key={item} />)
           : pets.map((pet) => (
               <CardsPets
-                key={pet.idPet}
-                id={pet.idPet}
-                img={pet.image.length > 0 ? converterBase64ToUrl(pet.image[0].imagePet) : NoPhoto}
-                name={pet.name}
+                key={pet?.id}
+                id={pet?.id}
+                img={pet.images.length > 0 ? pet.images[0].image : NoPhoto}
+                name={pet?.name}
                 gender={pet?.gender}
-                ubication={pet?.ubication}
+                ubication={pet?.ubicacion.country + ", " + pet?.ubicacion.city}
                 handleClickAdopt={handleClickAdopt}
               />
             ))}
@@ -235,13 +281,13 @@ const UserProfile = () => {
 };
 
 const modalStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 600,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
